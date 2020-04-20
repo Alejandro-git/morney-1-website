68b8b80116b9b03faf222a49becce2ff2f437d3d
Alejandro <836445882@qq.com>
2020-04-20 09:07:26 +0800

    deploy

---
+++ precache-manifest.18c57e2bb0ac37d7917e12bdb86783c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a9bc1aa955a756051d6",
+    "url": "/morney-1-website/css/app.e0163c04.css"
+  },
+  {
+    "revision": "099a03f1beb89fa9ccdee9dadb307162",
+    "url": "/morney-1-website/index.html"
+  },
+  {
+    "revision": "4a9bc1aa955a756051d6",
+    "url": "/morney-1-website/js/app.0aebd713.js"
+  },
+  {
+    "revision": "6f7465bcefe034e9989a",
+    "url": "/morney-1-website/js/chunk-vendors.b3b70078.js"
+  },
+  {
+    "revision": "b0c72820654d086f2b526db8c6a20387",
+    "url": "/morney-1-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-1-website/robots.txt"
+  }
+]);
